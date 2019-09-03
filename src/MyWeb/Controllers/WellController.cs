cbb4c868ff2885a5f27f18b9a3375feaac2446c1
wk-j <somnuk.wk@gmail.com>
2019-09-03 12:53:52 +0700

    Must return application/octet-stream

--- src/MyWeb/Controllers/WellController.cs
+++ src/MyWeb/Controllers/WellController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyWeb.Controllers
@@ -8,6 +9,19 @@ namespace MyWeb.Controllers
     {
         [HttpGet(".well-known/apple-app-site-association")]
         public IActionResult Get()
+        {
+            var file = "Files/apple-app-site-association";
+            var apple = new FileStream(file, FileMode.Open, FileAccess.Read);
+            var stream = new MemoryStream();
+            apple.CopyTo(stream);
+            stream.Position = 0;
+
+            var mimeType = "application/octet-stream";
+            return File(stream, mimeType, file);
+        }
+
+        [HttpGet(".well-known/apple-app-site-association-0")]
+        public IActionResult Get0()
         {
             return Ok(new
             {
